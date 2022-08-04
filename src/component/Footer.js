beadf2b09bd85816f05e19865ff2f68f2143d4a2
Vishal <vssule@mitaoe.ac.in>
2022-08-04 10:54:02 +0530

    react code

---
+++ src/component/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer =()=>{
+    return(
+        <div>
+            <center>
+                <hr/>
+                <h3>&copy; vsdeveloper</h3>
+                {/* <hr/> */}
+            </center>
+        </div>
+        
+    )
+}
+export default Footer;
---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Home=()=>{
+    return (
+        <div>
+            <Header/>
+            <h1>Hii From React</h1>
+            <he>Rect Code</he>
+            <Footer/>
+
+        </div>
+    
+    
+    )
+}
+export default Home;
